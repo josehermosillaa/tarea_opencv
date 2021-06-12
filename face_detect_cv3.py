4982a9d18fa0dcf964c5efbbe4de6f44e3e73307
Jose Hermosilla <jose.hermosillaasach.cl>
2021-06-12 11:57:49 -0400

    se ignora el webcam.log

--- face_detect_cv3.py
+++ face_detect_cv3.py
@@ -15,7 +15,7 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # Detect faces in the image
 faces = faceCascade.detectMultiScale(
     gray,
-    scaleFactor=1.1,
+    scaleFactor=1.035,
     minNeighbors=5,
     minSize=(30, 30)
     #flags = cv2.CV_HAAR_SCALE_IMAGE
@@ -24,8 +24,12 @@ faces = faceCascade.detectMultiScale(
 print("Found {0} faces!".format(len(faces)))
 
 # Draw a rectangle around the faces
+# for (x, y, w, h) in faces:
+#     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    center_coordinates = x + w // 2, y + h // 2
+    radius = w // 2 # or can be h / 2 or can be anything based on your requirements
+    cv2.circle(image, center_coordinates, radius, (0, 255, 0), 2)
 
 cv2.imshow("Faces found", image)
 cv2.waitKey(0)
